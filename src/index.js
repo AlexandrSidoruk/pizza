78af0101b3176b199778701d1784d61173a0520f
Alexandr Sidoruk <sidoruk.alexandr8@gmail.com>
2022-08-18 09:49:50 +0300

    react project pizza site

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from "./redux/store";
+
+
+import './scss/app.scss';
+
+import App from './App';
+import Provider from "react-redux/es/components/Provider";
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+        <Provider store={store}>
+      <App />
+        </Provider>>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
